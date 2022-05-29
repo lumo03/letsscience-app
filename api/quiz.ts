72dd1b8662190f228fa6522895b33921461c90e8
FinnRG <finngaertner@protonmail.com>
2022-05-29 18:30:55 +0200

    feat: Add serverless functions

---
+++ api/quiz.ts
@@ -0,0 +1,39 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+const quizzes = [{
+  id: 1,
+  title: "Mathematik",
+  questions: [
+    {
+      question: "1+2",
+      answers: ["1", "2", "3", "4"],
+      correctAnswer: 2,
+    }
+  ]
+},
+{
+  id: 2,
+  title: "Geographie",
+  questions: [
+    {
+      question: "Wie lautet die Hauptstadt von Deutschland",
+      answers: ["Berlin", "Köln", "Frankfurt", "Rom"],
+      correctAnswer: 0,
+    }
+  ]
+}
+]
+
+export default (request: VercelRequest, response: VercelResponse) => {
+  const { id } = request.query;
+
+  if (id instanceof Array) {
+    response.status(400);
+    return;
+  }
+
+  response
+    .setHeader('Content-Type', 'application/json')
+    .status(200)
+    .send(JSON.stringify(quizzes[id]));
+};
